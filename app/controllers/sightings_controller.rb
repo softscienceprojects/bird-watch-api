810275af731f0d8024f09e3ab0b79adfd44ee8b8
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-19 19:25:14 +0100

    updated routes and sightings controller

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,2 +1,7 @@
 class SightingsController < ApplicationController
+
+    def index
+        sightings = Sighting.all
+        render json: sightings, include: [:bird, :location]
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :sightings
-  resources :locations
-  resources :birds
+  resources :sightings, only: [:show, :index]
+  resources :locations, only: [:show, :index]
+  resources :birds, only: [:show, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
